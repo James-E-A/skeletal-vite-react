d3ab3ec9fe0c833cbf8712e50c95b60e0afcd8aa
James Edington Administrator <codegeek98@gmail.com>
2024-08-19 18:52:21 -0500

    fix component

--- main.mjs
+++ main.mjs
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom/client'
 import App from './lib-react/App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('body')); // this isn't best practice but I also don't care
-root.render(App());
+root.render(React.createElement(App));
 console.info(root);
