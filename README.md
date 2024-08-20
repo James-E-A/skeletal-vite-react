0a3d2f3a1138c8fa3d71ab0d4141fb3291946fa2
James Edington Administrator <codegeek98@gmail.com>
2024-08-20 10:08:05 -0500

    static hosting safe dist

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 npm install
 git commit -m "Update dependencies" -- "package-lock.json"
 npm exec -- vite dev
+npm exec -- vite build --emptyOutDir
 ```
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	root: 'src',
+	build: {
+		outDir: "../dist", // don't put this inside src
+		rollupOptions: {
+			output: {
+				preserveModules: true,
+			},
+			preserveEntrySignatures: "allow-extension", // required by preserveModules
+		},
+	},
 	plugins: [
 		react(),
 	],
+	root: "src",
 })
