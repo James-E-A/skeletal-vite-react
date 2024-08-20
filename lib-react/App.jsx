803ac4c1325f02629ee0632d8122e12ea8225161
James Edington Administrator <codegeek98@gmail.com>
2024-08-19 19:05:38 -0500

    state demo

--- lib-react/App.jsx
+++ lib-react/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+
 const App = () => {
+	const [titleText, setTitleText] = useState("Meme");
+	setTimeout(() => {
+		setTitleText("chungus");
+	}, 1000);
 	return [
-		<title>Meme</title>,
+		<title>{titleText}</title>,
 		<h1>WOW IT'S LESS THAN 50 SLOC IN THIS REPO</h1>,
 		<p>(Probably... I didn't count it lol)</p>
 	];
