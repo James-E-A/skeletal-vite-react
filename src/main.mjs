3a2e336107f6e0cdbdb8118a4fe2dbc03f3a7c94
James Edington Administrator <codegeek98@gmail.com>
2024-08-20 09:12:18 -0500

    housekeeping

--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './lib-react/App.jsx'
+import App from './components/App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('body')); // this isn't best practice but I also don't care
 root.render(React.createElement(App));
