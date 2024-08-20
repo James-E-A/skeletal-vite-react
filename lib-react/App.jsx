9bc4904b6243508e76063884c4297a88c3a45fdd
James Edington Administrator <codegeek98@gmail.com>
2024-08-19 23:44:31 -0500

    minor changes

--- lib-react/App.jsx
+++ lib-react/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 
 const App = () => {
-	const [titleText, setTitleText] = useState("Meme");
+	const [titleText, setTitleText] = useState("bool");
+	// hot module reloading will re-run this block of code,
+	// but it will NOT restore the initial "bool" state, even temporarily
+	console.debug({ titleText });
 	setTimeout(() => {
 		setTitleText("chungus");
 	}, 1000);
 	return [
-		<title>{titleText}</title>,
+		<title>{`Meme: ${titleText}`}</title>,
 		<h1>WOW IT'S LESS THAN 50 SLOC IN THIS REPO</h1>,
 		<p>(Probably... I didn't count it lol)</p>
 	];
--- main.mjs
+++ main.mjs
@@ -5,4 +5,4 @@ import App from './lib-react/App.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('body')); // this isn't best practice but I also don't care
 root.render(React.createElement(App));
-console.info(root);
+console.debug(root);
